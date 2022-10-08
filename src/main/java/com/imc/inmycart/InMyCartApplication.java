422ed7f6f97e9644a1be4d416ebe0412b833cf69
mina <alsk464@gmail.com>
2022-10-08 16:29:57 +0900

    [MINA] 프로젝트 생성

--- src/main/java/com/imc/inmycart/InMyCartApplication.java
+++ src/main/java/com/imc/inmycart/InMyCartApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class InMyCartApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(InMyCartApplication.class, args);
 
